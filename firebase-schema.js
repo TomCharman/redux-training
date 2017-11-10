f8ef036024c1c2b26113e0016f1e505dbb7c96c2
Tom Charman <tom@thegreatescapism.com>
2017-11-11 09:32:19 +1100

    Hello world commit

---
+++ firebase-schema.js
@@ -0,0 +1,29 @@
+Home
+  All Ducks
+
+Profile
+  User info
+  Users Ducks
+
+Replies
+  Specific Duck
+  Ducks Replies
+
+/users
+  uid
+    ducks
+      duckID
+        replies
+          replyId
+            uid
+            name
+            comment
+            etc
+        info
+          uid
+          avatar
+          text
+          numberOfLikes
+          etc
+    likes
+      duckId
